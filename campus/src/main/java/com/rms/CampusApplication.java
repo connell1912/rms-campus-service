face37162d2edfe70ad490a271eef92aaaaad4d9
Connor Casey <connorcaseyc@gmail.com>
2020-02-27 23:33:11 -0500

    working on rooms

--- campus/src/main/java/com/rms/CampusApplication.java
+++ campus/src/main/java/com/rms/CampusApplication.java
@@ -9,7 +9,6 @@ public class CampusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CampusApplication.class, args);
-
 	}
 
 	
--- campus/src/main/java/com/rms/controller/RoomController.java
+++ campus/src/main/java/com/rms/controller/RoomController.java
@@ -4,6 +4,7 @@ import com.rms.dao.RoomDao;
 import com.rms.model.Room;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/room")
+@CrossOrigin(origins={"http://localhost:3000"})
 public class RoomController {
     
     @Autowired

282a2b5950b24f333f2f40bc48f8c768edc4bed9
{connorcaseyc} <{connorcaseyc@gmail.com}>
2020-03-04 09:20:12 -0500

    adding some testing

--- campus/src/test/java/com/rms/service/RoomServiceTest.java
+++ campus/src/test/java/com/rms/service/RoomServiceTest.java
@@ -79,12 +79,77 @@ public class RoomServiceTest {
         System.out.println(room);
         when(rd.findById(1)).thenReturn(Optional.of(room));
         Room r2 = rs.findRoomById(1);
-        r2.setRoomNumber("1234a");
+        r2.setRoomNumber("NOT1234");
         rs.update(r2);
-        System.out.println(r2);
+        System.out.println(r2.getRoomNumber());
+        // assertFalse(r2.getRoomNumber().equals(room.getRoomNumber()));
         assertTrue(room.getRoomNumber().equals(r2.getRoomNumber()));
     }
 
     
+
+
+    ////////////////////////////////////////////////////////////
+    @Test
+    public void testSaveNewAddress() {
+        Address test = new Address("test-street", "test-city", "test-state", "test-zip", "test-country");
+        as.save(test);
+        verify(ad, times(1)).save(test);
+    }
+
+    @Test
+    public void testFindAddressById() {
+        when(ad.findById(1))
+                .thenReturn(new Address("test-street", "test-city", "test-state", "test-zip", "test-country"));
+        Address add = as.findAddressById(1);
+        assertEquals("test-street", add.getUnitStreet());
+        assertEquals("test-city", add.getCity());
+        assertEquals("test-state", add.getState());
+    }
+
+    @Test
+    public void testReadAllAddresses() {
+        Iterable<Address> add = as.findAll();
+        assertNotNull(add);
+    }
+
+    @Test
+    public void testUpdateAddress() {
+        Address add = new Address("test-street", "test-city", "test-state", "test-zip", "test-country");
+        as.save(add);
+        Address add2 = as.findAddressById(1);
+        add2 = new Address("test-street2", "test-city", "test-state", "test-zip", "test-country");
+        as.update(add2);
+        assertEquals("test-street2", add2.getUnitStreet());
+    }
+
+    @Test
+    public void testDeleteAddress() {
+        Address delete = new Address(0, "delete-street", "delete-city", "delete-state", "delete-zip", "delete-country");
+        as.save(delete);
+        Address add2 = as.findAddressById(1);
+        as.delete(add2);
+        Address add3 = as.findAddressById(1);
+        assertFalse(add3 != null);
+    }
+
+    @Test
+    public void testUpdateById() {
+        Address add = new Address("test-street", "test-city", "test-state", "test-zip", "test-country");
+        as.save(add);
+        Address add2 = as.findAddressById(1);
+        add2 = new Address("test-street2", "test-city", "test-state", "test-zip", "test-country");
+        as.updateById(1);
+        assertEquals("test-street2", add2.getUnitStreet());
+    }
+
+    @Test
+    public void testDeleteById(){
+        Address add = new Address("test-street", "test-city", "test-state", "test-zip", "test-country");
+        as.save(add);
+        as.deleteById(1);
+        Address add2 = as.findAddressById(1);
+        assertFalse(add2 != null);
+    }
 
 }

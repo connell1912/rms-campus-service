3d210e73025951cf1af7eb34072b632d4e2f0dc9
dortega14 <dave.ort14@gmail.com>
2020-03-06 11:49:43 -0500

    DTOs are ready for testing

--- campus/src/main/java/com/rms/CampusApplication.java
+++ campus/src/main/java/com/rms/CampusApplication.java
@@ -5,6 +5,7 @@ import com.rms.facade.AddressFacade;
 import com.rms.facade.AmenityFacade;
 import com.rms.facade.BuildingFacade;
 import com.rms.facade.CampusFacade;
+import com.rms.facade.RMDFacade;
 import com.rms.facade.RoomFacade;
 import com.rms.facade.RoomStatusFacade;
 import com.rms.model.Address;
@@ -46,6 +47,11 @@ public class CampusApplication {
 	@Bean
 	public CampusFacade campusFacade() {
 		return new CampusFacade();
+	}
+
+	@Bean
+	public RMDFacade rmdFacade() {
+		return new RMDFacade();
 	}
 
 	@Bean
